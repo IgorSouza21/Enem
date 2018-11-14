3d0728c6671c64fbde1e7fff0143868587b236ef
MrAssis <pedrojudo74@hotmail.com>
2018-11-14 17:58:03 -0300

    Mudando de tela 1

--- app/src/main/java/com/example/enem/MainActivity.kt
+++ app/src/main/java/com/example/enem/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.example.enem
 
+import android.content.Intent
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enem)
+
+        var btnMat = findViewById(R.id.btMat) as Button
+
+        btnMat.setOnClickListener{
+            val telaNova = Intent(this,TelaDisciplina::class.java)
+            startActivity(telaNova)
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/example/enem/TelaDisciplina.kt
@@ -0,0 +1,12 @@
+package com.example.enem
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaDisciplina : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_disciplina)
+    }
+}
