520596640abd887f02f2fee13dd2a20b6bb82165
IgorSouza21 <igor11souza@gmail.com>
2018-11-14 00:04:59 -0300

    activity simulado, porém vazia e início da internacionalização

---
+++ app/src/main/java/com/example/enem/SimuladoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.enem
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SimuladoActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_simulado)
+    }
+}
