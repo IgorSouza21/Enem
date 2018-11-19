29f57a6bd778dc5fd478addc06b8691b2ab4fa65
MrAssis <pedrojudo74@hotmail.com>
2018-11-19 16:04:34 -0300

    telaDisciplina alterada

--- app/src/main/java/com/example/enem/MainActivity.kt
+++ app/src/main/java/com/example/enem/MainActivity.kt
@@ -1,16 +1,11 @@
 package com.example.enem
 
 import android.content.Intent
-import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.ImageView
 
-class MainActivity : AppCompatActivity(){
-
-
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +15,7 @@ class MainActivity : AppCompatActivity(){
 
         btnMat.setOnClickListener{
             val telaNova = Intent(this,TelaDisciplina::class.java)
+            telaNova.putExtra("disciplina","matematica")
             startActivity(telaNova)
         }
 
--- app/src/main/java/com/example/enem/TelaDisciplina.kt
+++ app/src/main/java/com/example/enem/TelaDisciplina.kt
@@ -2,11 +2,20 @@ package com.example.enem
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class TelaDisciplina : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_disciplina)
+        var tvDisc = findViewById<TextView>(R.id.tvDisciplina)
+        var disc = intent.getStringExtra("disciplina")
+        if (disc == "matematica"){
+            print(1)
+        }else{
+            print(2)
+        }
+
     }
 }
